dee0733431d5bffdfd95045bb7d06468b1248812
Nicholas <mail@nicholasgoga.com>
2023-12-03 15:18:15 +0100

    ADD mergetest file

--- src/mergetest.cpp
+++ src/mergetest.cpp
@@ -20,7 +20,15 @@ int analogBuffer[SCOUNT];    // store the analog value in the array, read from A
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
 float averageVoltage = 0,tdsValue = 0,temperature = 25;
- 
+
+//testmergeleds
+                int greenLedPin = 13; // Pin connected to the green LED
+                int yellowLedPin = 12; // Pin connected to the yellow LED
+                int redLedPin = 11; // Pin connected to the red LED
+
+
+
+
 //waterflowsensor
 volatile double waterFlow;
  
@@ -68,6 +76,11 @@ void setup() {
   lightMeter.begin();
  
   Serial.println(F("BH1750 Test begin"));
+
+//testmerge
+                    pinMode(greenLedPin, OUTPUT); // Set the green LED pin as output
+                    pinMode(yellowLedPin, OUTPUT); // Set the yellow LED pin as output
+                    pinMode(redLedPin, OUTPUT); // Set the red LED pin as output
  
 }
  
@@ -93,6 +106,31 @@ void WaterKwaliteit() {
       float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
       tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
     } 
+// TEST KLEURCODE LEDS WATERQUALI
+    if (tdsValue > 15 && tdsValue <= 50) {
+        digitalWrite(greenLedPin, HIGH); // Turn on the green LED
+        digitalWrite(yellowLedPin, LOW); // Turn off the yellow LED
+        digitalWrite(redLedPin, LOW); // Turn off the red LED
+        delay(500); // Delay for 500 milliseconds
+    } else if (tdsValue > 50 && tdsValue <= 100) {
+        digitalWrite(greenLedPin, LOW); // Turn off the green LED
+        digitalWrite(yellowLedPin, HIGH); // Turn on the yellow LED
+        digitalWrite(redLedPin, LOW); // Turn off the red LED
+        delay(1000); // Delay for 1000 milliseconds
+    } else if (tdsValue > 100) {
+        digitalWrite(greenLedPin, LOW); // Turn off the green LED
+        digitalWrite(yellowLedPin, LOW); // Turn off the yellow LED
+        digitalWrite(redLedPin, HIGH); // Turn on the red LED
+        delay(2000); // Delay for 2000 milliseconds
+    } else {
+        digitalWrite(greenLedPin, LOW); // Turn off the green LED
+        digitalWrite(yellowLedPin, LOW); // Turn off the yellow LED
+        digitalWrite(redLedPin, LOW); // Turn off the red LED
+        delay(200); // Delay for 200 milliseconds
+    }
+
+
+
 }
 int getMedianNum(int bArray[], int iFilterLen) 
 {
